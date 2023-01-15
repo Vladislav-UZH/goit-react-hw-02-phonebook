1820f935d51ba80a7c3884380a7c77b1de604e60
Vladislav-UZH <perebziak.vladislav@gmail.com>
2023-01-15 20:58:47 +0200

    fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,4 @@
-import Section from 'components/Section';
-import FeedbackOptions from 'components/FeedbackOptions';
-import Statistics from 'components/Statistics';
 import { Component } from 'react';
-import Notification from 'components/Notification';
 
 export class App extends Component {
   state = {
