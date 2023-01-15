1533a2be19340b622d70bd7b676073339aa4186c
Vladislav-UZH <perebziak.vladislav@gmail.com>
2023-01-16 00:01:17 +0200

    add two new methods to create and delete contacts

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,13 +1,50 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
 
+import ContactsForm from 'components/ContactsForm';
+import ContactsList from 'components/ContactsList/ContactsList';
 export class App extends Component {
   state = {
-    good: 0,
-    neutral: 0,
-    bad: 0,
+    contacts: [
+      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+    ],
+    name: '',
   };
-
+  createContact = ({ name, number = '' }) => {
+    // console.log(data);
+    const id = nanoid();
+    this.setState(prevState => {
+      return { name, contacts: [{ name, number, id }, ...prevState.contacts] };
+    });
+  };
+  deleteContact = id => {
+    this.setState(prevState => {
+      return {
+        contacts: prevState.contacts.filter(contact => contact.id !== id),
+      };
+    });
+    console.log('delete');
+  };
   render() {
-    return;
+    return (
+      <div
+        style={{
+          padding: 20,
+          display: 'flex',
+          justifyContent: 'center',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <ContactsForm createContact={this.createContact} />
+        <ContactsList
+          deleteContact={this.deleteContact}
+          contacts={this.state.contacts}
+        />
+      </div>
+    );
   }
 }
